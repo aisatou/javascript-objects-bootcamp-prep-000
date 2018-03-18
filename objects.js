acdc6543a093145d6c5d378521b018c846569bab
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-03-18 01:49:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,10 +1,7 @@
 var playlist = { music: "song" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var music = {artistName: songTitle}
-  music.artistName = "Phil Ochs"
-  music.songTitle = "Here's to the State of Mississippi"
-  return music;
+  
 }
 
 function removeFromPlaylist(playlist, artistName) {
